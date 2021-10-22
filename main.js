a9b7fc71f6d579183aa8f53935026cf1b4eb3a86
lyh7712 <mmj2rv9@kakao.com>
2021-10-22 13:23:02 +0900

    use toggle

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
-const title = document.querySelector(".hello h1");
+const title = document.querySelector("div.hello h1");
 
-console.log(title);
-
-function handleClickEvent() {
-    console.log("clicked title");
-    title.style.color = "red";
+function handleClick() {
+    const clickedClass = "clicked";
+    
+    title.classList.toggle(clickedClass);
 }
 
-function handleMouseEnter() {
-    title.innerHTML = "mouse is here";
-}
-
-function handleMouseLeave() {
-    title.innerHTML = "mouse is gone";
-}
-
-title.addEventListener("click", handleClickEvent);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+title.addEventListener("click", handleClick);
